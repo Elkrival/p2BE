b4a9594bd5486645349a91d0dea31255933981cc
Ivan Trujillo <krival1987@gmail.com>
2016-07-25 17:51:58 -0400

    Favorites button and display button complete

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ PORT = process.env.PORT || 3000;
 var UNOG_SKEY = process.env.UNOGS_KEY;
 
 //naming the collection to store the database
-var NETFLIX_N_CHILL_COLLECTION = "/netflix";
+var NETFLIX_N_CHILL_COLLECTION = "netflix";
 //setting app variable to express
 var app = express();
 //nominating the express tools to use such as body-parser and cors
 //body parser translates our data cors will allow cross origin sync
 app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }));
+app.use(bp.urlencoded({extended: true}));
 app.use(cors());
 //connecting mongo server to database, port 27017 also adding error handling which
 //starts after url line
-// var url = 'mongodb://localhost:27017/netflix_project'; //local testing
-var url = 'mongodb://heroku_h7pzl3ch:16rnh913p50naacq4n3oibq2r@ds029705.mlab.com:29705/heroku_h7pzl3ch' //production
+var url = 'mongodb://localhost:27017/netflix_project'; //local testing
+//var url = 'mongodb://heroku_h7pzl3ch:16rnh913p50naacq4n3oibq2r@ds029705.mlab.com:29705/heroku_h7pzl3ch' //production
 //port that we will use mongo
-mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function(err, database) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function (err, database) {
 
     //naming our database
     db = database;
@@ -40,9 +40,20 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function(err, databa
 //HARD PART
 //We need to GET searches from API to save them
 //We need to create new events to our database
+app.get('/netflix/new', function (req, res) {
+    console.log(req.params)
+    db.collection(NETFLIX_N_CHILL_COLLECTION).find({}).toArray(function (error, result) {
+        if (error) {
+            console.log('No movies here!', error);
+            res.json('No movies here');
+        } else {
+            // console.log('Displaying Movies' + res.json(result));******DO NOT DO THIS, THIS IS A METHOD********
+            res.json(result);
+        }
+    })
+})
 
-
-app.post('/netflix/search', function(req, res) {//for this to work REQ is before RES
+app.post('/netflix/search', function (req, res) {//for this to work REQ is before RES
     var inputValue = req.body.input;
     unirest.get("https://unogs-unogs-v1.p.mashape.com/api.cgi?q=" + inputValue + "-!1900,2017-!0,5-!0,10-!0-!Any-!Any-!Any-!gt100&t=ns&cl=all&st=adv&ob=Relevance&p=1&sa=and")
         .header("X-Mashape-Key", UNOG_SKEY)
@@ -53,26 +64,13 @@ app.post('/netflix/search', function(req, res) {//for this to work REQ is before
             console.log(res.send)
         })
 });
-// app.get("/netflix/new", function(res, req) {
-//
-//     // find all contacts and return them as an array
-//     db.collection(NETFLIX_N_CHILL_COLLECTION).find({}).toArray(function(err, docs) {
-//         if (err) {
-//             handleError(res, err.message, "Couldn't get Chill movies");
-//         } else {
-//             res.status(200).json(docs);
-//         }
-//     });
-// });
-//
-// });
-app.post("/netflix/new", function(req, res) {
+app.post("/netflix/new", function (req, res) {
 
     var newMovie = req.body
     console.log(newMovie)
     console.log(typeof newMovie)
     // insert one new contact
-    db.collection(NETFLIX_N_CHILL_COLLECTION).insertOne(newMovie, function(err, doc) {
+    db.collection(NETFLIX_N_CHILL_COLLECTION).insertOne(newMovie, function (err, doc) {
         if (err) {
             handleError(res, err.message, "Failed to create new contact.");
         } else {
@@ -80,7 +78,6 @@ app.post("/netflix/new", function(req, res) {
         }
     });
 });
-
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log('listen to events on a "port: ', PORT)
 });
